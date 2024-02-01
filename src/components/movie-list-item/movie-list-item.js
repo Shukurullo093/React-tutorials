a21f171e520b08fe6c46979823bd6181b7323129
Shukurullo093 <nazarqosimovshukurullo093@gmail.com>
2024-02-01 16:22:20 +0500

    10-lesson

---
+++ src/components/movie-list-item/movie-list-item.js
@@ -0,0 +1,23 @@
+import './movie-list-item.css'
+
+const MovieListItem = () => {
+  return (
+    <li className='list-group-item d-flex justify-content-between'>
+        <span className='list-group-item-label'>Terminator</span>
+        <input type='number' className='list-group-item-input' defaultValue={100} />
+        <div className='d-flex justify-content-center align-items-center'>
+            <button type='button' className='btn-cookie btn-sm'>
+                <i className='fas fa-cookie'></i>
+            </button>
+            <button type='button' className='btn-trash btn-sm'>
+                <i className='fas fa-trash'></i>
+            </button>
+            <button type='button' className='btn-star btn-sm'>
+                <i className='fas fa-star'></i>
+            </button>
+        </div>
+    </li>
+  )
+}
+
+export default MovieListItem
