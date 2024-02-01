9223e6b313fbdd79ed8f0445e5e49545f1499a6c
Shukurullo093 <nazarqosimovshukurullo093@gmail.com>
2024-02-01 10:06:27 +0500

    8-lesson

---
+++ src/components/search-panel/search-panel.js
@@ -0,0 +1,9 @@
+import './search-panel.css'
+
+const SearchPanel = () => {
+  return <div>
+        <input type='text' className='form-control search-input' placeholder="Kinolarni qidirish..." />
+    </div>
+}
+
+export default SearchPanel
--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
-import React from 'react';
+// import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import './index.css';
+
+import App from './components/app/app';
 
 ReactDOM.render(
   <App />,
   document.getElementById('root')
 );
+
+// rafce
